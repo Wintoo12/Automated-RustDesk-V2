03681417484b0ba5c889deae646cb6f7c6240ac3
Wintoo12 <sencilmarkandrew12@gmail.com>
2025-02-07 13:40:08 +0800

    Final

--- script.js
+++ script.js
@@ -1,6 +1,40 @@
-function openRustdesk() {
-  fetch("/open-rustdesk")
-    .then((response) => response.text())
-    .then((message) => console.log(message))
-    .catch((error) => console.error("Error:", error));
+function showAlert(message) {
+  let alertBox = document.querySelector(".alert");
+  alertBox.innerText = message;
+  alertBox.style.display = "block"; // Show the alert
+
+  setTimeout(() => {
+    alertBox.style.display = "none"; // Hide after 5 seconds
+  }, 5000);
+}
+
+function copyPasswordAnyDesk() {
+  let password = document.getElementById("adPassword").innerText;
+  navigator.clipboard
+    .writeText(password)
+    .then(() => {
+      showAlert("Password copied! Redirecting to AnyDesk...");
+      setTimeout(() => {
+        window.location.href = "AnyDesk://488586374?password=a7e8b6929485";
+      }, 3000); // Redirect after 3 seconds
+    })
+    .catch((err) => {
+      console.error("Failed to copy:", err);
+    });
+}
+
+function copyPasswordRustDesk() {
+  let password = document.getElementById("rdPassword").innerText;
+  navigator.clipboard
+    .writeText(password)
+    .then(() => {
+      alert("Password copied! Just paste it in RustDesk.");
+    })
+    .catch((err) => {
+      console.error("Failed to copy:", err);
+    });
+}
+
+function openRustDesk() {
+  window.location.href = "rustdesk://117704810?password=raspberry1";
 }
